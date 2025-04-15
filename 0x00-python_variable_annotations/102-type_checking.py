11cc1890fba490c0835aa3d2ee99cbec6d3b4dd8
Ken Wekesa <wanyamak884@gmail.com>
2025-04-15 15:59:03 +0300

    mypy validation

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,13 +7,13 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """Use mypy to validate the following piece of code and apply any necessary
     changes.
     """
-    zoomed_in: Tuple = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
