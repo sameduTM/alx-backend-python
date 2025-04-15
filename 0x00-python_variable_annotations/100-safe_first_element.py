0333e9e1f851c32516bed45071f98ef4375f5a36
Ken Wekesa <wanyamak884@gmail.com>
2025-04-15 12:44:25 +0300

    Use code that works with python3.7

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Duck typing - first element of a sequence"""
-from typing import Sequence, Any, Union, Optional
-from types import NoneType
+from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """The types of the elements of the input are not know
     """
     if lst:
