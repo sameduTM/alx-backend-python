92da46af407e1ae44b9a3992511ed8fdebaa754c
Ken Wekesa <wanyamak884@gmail.com>
2025-04-15 15:21:09 +0300

    More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,30 +1,15 @@
 #!/usr/bin/env python3
 """Given the parameters and the return values, add type annotations to the
-function
-
-Hint: look into TypeVar
-
-"""
-from typing import TypeVar, Dict, Optional
-
-KT = TypeVar('KT')  # Type variable for dictionary keys
-VT = TypeVar('VT')  # Type variable for dictionary values
+function"""
+from typing import Mapping, Any, TypeVar, Union
 
 
-def safely_get_value(dct: Dict[KT, VT], key: KT,
-                     default: Optional[VT] = None) -> Optional[VT]:
-    """Safely get a value from a dictionary `dct` based on `key`.
+T = TypeVar("T")
 
-    Args:
-        dct (Dict[KT, VT]): The dictionary-like object.
-        key (KT): The key to lookup in `dct`.
-        default (Optional[VT], optional): Default value to return if `key` is
-        not found (default is None).
 
-    Returns:
-        Optional[VT]: The value associated with `key` in `dct`, or
-        `default` if `key` is not found.
-    """
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None]) -> Union[Any, T]:
+    """safely get value"""
     if key in dct:
         return dct[key]
     else:
