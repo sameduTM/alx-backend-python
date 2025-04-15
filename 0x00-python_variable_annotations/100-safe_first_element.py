54b491c727dc31103308f310d9a114e79a8a6ccd
Ken Wekesa <wanyamak884@gmail.com>
2025-04-15 12:42:31 +0300

    Everithing is documented

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""Duck typing - first element of a sequence"""
 from typing import Sequence, Any, Union, Optional
 from types import NoneType
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+    """The types of the elements of the input are not know
+    """
     if lst:
         return lst[0]
     else:
