0655bdfe94614fda3b2610071c37398eb69ad2e5
Ken Wekesa <wanyamak884@gmail.com>
2025-04-17 16:46:31 +0300

    annotate function to return value with appropriate type

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,12 @@
 """Let's execute multiple coroutines at the same time with async
 """
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """return the list of all the delays (float values)"""
     delays_list = []
     for _ in range(n):
