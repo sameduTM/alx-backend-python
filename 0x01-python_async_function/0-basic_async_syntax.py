899e5892bf066aae64d8923615a6c2738b3f4618
Ken Wekesa <wanyamak884@gmail.com>
2025-04-17 12:51:44 +0300

    add annotation for the function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """synchronous coroutine that takes in an integer argument (max_delay,
     with a default value of 10) named wait_random that waits for a random
     delay between 0 and max_delay (included and float value) seconds and
--- 0x01-python_async_function/0-main.py
+++ 0x01-python_async_function/0-main.py
@@ -7,3 +7,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 print(asyncio.run(wait_random()))
 print(asyncio.run(wait_random(5)))
 print(asyncio.run(wait_random(15)))
+print(wait_random.__annotations__)
