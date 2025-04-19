7cb7f122dd7127defbd28d1117276709e0d214b1
Ken Wekesa <wanyamak884@gmail.com>
2025-04-20 01:03:05 +0300

    pycodestyle validation - remove trailing space

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,9 +7,8 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """The coroutine will loop 10 times, each time asynchronously wait 1
-    second, then yield a random number between 0 and 10. Use the random 
-    module.
+    """The coroutine will loop 10 times, each time asynchronously wait 1 sec
+    then yield a random number between 0 and 10. Use the random module.
     """
     for _ in range(10):
         await asyncio.sleep(1)
