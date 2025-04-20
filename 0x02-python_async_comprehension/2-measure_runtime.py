f2dcc800f36f1e950685c3fb48ba975b87278351
Ken Wekesa <wanyamak884@gmail.com>
2025-04-20 15:42:53 +0300

    annotate function to return value with appropriate type

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """coroutine that will execute async_comprehension four times in parallel
     using asyncio.gather.
     """
