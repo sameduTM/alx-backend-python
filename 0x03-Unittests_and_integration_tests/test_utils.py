31b582baa8d789ffc9dad369c083d5219e9a419d
Ken Wekesa <wanyamak884@gmail.com>
2025-07-17 10:51:07 +0300

    pycodestyle

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -65,7 +65,8 @@ class TestMemoize(unittest.TestCase):
                 """a_property"""
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+        with patch.object(TestClass, 'a_method', return_value=42) as\
+                mock_method:
             obj = TestClass()
             result1 = obj.a_property
             result2 = obj.a_property
